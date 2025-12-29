3c67508e7d4ef22c9c710a7fa7a080e6dbbab904
zacksloth5 <zackgourevitch@gmail.com>
2025-12-29 15:52:36 -0500

    Made traps terrible

--- script/outside.js
+++ script/outside.js
@@ -96,32 +96,32 @@ var Outside = {
 	},
 	TrapDrops: [
 		{
-			rollUnder: 0.5,
+			rollUnder: 0,
 			name: 'fur',
 			message: _('scraps of fur')
 		},
 		{
-			rollUnder: 0.75,
+			rollUnder: 0,
 			name: 'meat',
 			message: _('bits of meat')
 		},
 		{
-			rollUnder: 0.85,
+			rollUnder: 0,
 			name: 'scales',
 			message: _('strange scales')
 		},
 		{
-			rollUnder: 0.93,
+			rollUnder: 0,
 			name: 'teeth',
 			message: _('scattered teeth')
 		},
 		{
-			rollUnder: 0.995,
+			rollUnder: 0,
 			name: 'cloth',
 			message: _('tattered cloth')
 		},
 		{
-			rollUnder: 1.0,
+			rollUnder: 1,
 			name: 'charm',
 			message: _('a crudely made charm')
 		}
